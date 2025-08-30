6f39c9ab66664ca37c7adf811341146178770240
RashedAkash <rashedhossain416400@gmail.com>
2025-08-30 17:55:46 +0600

    blog details create

--- wp-content/themes/airvice/template-parts/content-gallery.php
+++ wp-content/themes/airvice/template-parts/content-gallery.php
@@ -1,3 +1,51 @@
+<?php if(is_single()): ?>
+    <?php 
+$gallery = function_exists('get_field')? get_field('image_url'): ' ';
+// var_dump($gallery)
+?>
+  <div class="ablog ablog-4 mb-55">
+                        <div class="ablog__img">
+                        <div class="ablog__img--active swiper-container">
+                            <div class="swiper-wrapper">
+                            <?php if ( !empty($gallery) && is_array($gallery) ) : ?>
+                        <?php foreach( $gallery as $image ) : ?>
+                            <div class="ablog__img--item swiper-slide">
+                                <img src="<?php echo esc_url($image['full_image_url']); ?>" 
+                                    alt="img" 
+                                    class="img-fluid">
+                            </div>
+                        <?php endforeach; ?>
+                        <?php endif; ?>
+
+                            </div>
+                            <!-- If we need navigation buttons -->
+                            <div class="swiper-button-prev slide-prev"><i class="far fa-chevron-left"></i></div>
+                            <div class="swiper-button-next slide-next"><i class="far fa-chevron-right"></i></div>
+                        </div>
+                    </div>
+    <div class="ablog__text ablog__text4">
+        <div class="ablog__meta ablog__meta4">
+            <ul>
+                <li><a href="<?php the_permalink(); ?>"><i class="far fa-calendar-check"></i>  <?php echo get_the_date(); ?> </a></li>
+                <li><a href="<?php the_permalink(); ?>"><i class="far fa-user"></i> <?php the_author(); ?> </a></li>
+                <li>  <a href="<?php comments_link(); ?>">
+                <i class="fal fa-comments"></i> 
+                <?php comments_number( 'No Comments', '1 Comment', '% Comments' ); ?>
+            </a></li>
+            </ul>
+        </div>
+        <h3 class="ablog__text--title4 mb-20"><?php the_title(); ?></h3>
+        <div class='mb-30'>
+            <?php the_content(); ?>
+        </div>
+        <div class="blog__deatails--tag wow fadeInUp">
+            <span><?php echo esc_html__('Post Tags :') ?></span>
+            <?php airvice_tags(); ?>
+        </div>
+    </div>
+</div>
+
+<?php else: ?>
 <?php 
 $gallery = function_exists('get_field')? get_field('image_url'): ' ';
 // var_dump($gallery)
@@ -61,3 +109,4 @@ $gallery = function_exists('get_field')? get_field('image_url'): ' ';
         </div>
     </div>
 </div>
+<?php endif; ?>
--- wp-content/themes/airvice/template-parts/content-video.php
+++ wp-content/themes/airvice/template-parts/content-video.php
@@ -1,14 +1,16 @@
-<?php 
-$video = function_exists('get_field')? get_field('post_format'): ' ';
-?>
+<?php if ( is_single() ) : ?>
 
-<div id="post-<?php the_ID(); ?>" <?php post_class('ablog ablog-4 mb-55 wow fadeInUp'); ?> data-wow-delay=".4s">
-     <div class="ablog__img ablog__img4">
+    <?php 
+    $video = function_exists('get_field') ? get_field('post_format') : '';
+    ?>
+    
+    <div class="ablog__img ablog__img4">
         <?php if ( has_post_thumbnail() ) : ?>
             <a href="<?php the_permalink(); ?>">
                 <?php the_post_thumbnail('large'); ?>
             </a>
         <?php endif; ?>
+
         <?php if ( !empty($video) ) : ?>
             <div class="avideo-btn-4">
                 <div class="avideo-btn play_btn">
@@ -18,15 +20,15 @@ $video = function_exists('get_field')? get_field('post_format'): ' ';
                 </div>
             </div>
         <?php endif; ?>
-</div>
-    
+    </div>
+
     <div class="ablog__text ablog__text4">
         <div class="ablog__meta ablog__meta4">
             <ul>
                 <li>
                     <a href="<?php the_permalink(); ?>">
                         <i class="far fa-calendar-check"></i> 
-                        <?php echo get_the_date(); ?>
+                        <?php echo esc_html( get_the_date() ); ?>
                     </a>
                 </li>
                 <li>
@@ -38,22 +40,85 @@ $video = function_exists('get_field')? get_field('post_format'): ' ';
                 <li>
                     <a href="<?php comments_link(); ?>">
                         <i class="fal fa-comments"></i> 
-                        <?php comments_number( 'No Comments', '1 Comment', '% Comments' ); ?>
+                        <?php comments_number( esc_html__('No Comments', 'airvice'), esc_html__('1 Comment', 'airvice'), esc_html__('% Comments', 'airvice') ); ?>
                     </a>
                 </li>
             </ul>
         </div>
 
-        <h3 class="ablog__text--title4 mb-20">
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-        </h3>
+        <h3 class="ablog__text--title4 mb-20"><?php the_title(); ?></h3>
 
-        <?php the_excerpt(); ?>
+        <div class="mb-30">
+            <?php the_content(); ?>
+        </div>
 
-        <div class="ablog__btn4">
-            <a href="<?php the_permalink(); ?>" class="theme-btn">
-                <?php echo esc_html__('Read More', 'airvice'); ?>
-            </a>
+        <div class="blog__deatails--tag wow fadeInUp">
+            <span><?php echo esc_html__('Post Tags :', 'airvice'); ?></span>
+            <?php airvice_tags(); ?>
         </div>
     </div>
-</div>
+
+<?php else: ?>
+
+    <?php 
+    $video = function_exists('get_field') ? get_field('post_format') : '';
+    ?>
+
+    <div id="post-<?php the_ID(); ?>" <?php post_class('ablog ablog-4 mb-55 wow fadeInUp'); ?> data-wow-delay=".4s">
+        <div class="ablog__img ablog__img4">
+            <?php if ( has_post_thumbnail() ) : ?>
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail('large'); ?>
+                </a>
+            <?php endif; ?>
+
+            <?php if ( !empty($video) ) : ?>
+                <div class="avideo-btn-4">
+                    <div class="avideo-btn play_btn">
+                        <a class="venobox" data-autoplay="true" data-vbtype="video" href="<?php echo esc_url($video); ?>">
+                            <i class="fas fa-play"></i>
+                        </a>
+                    </div>
+                </div>
+            <?php endif; ?>
+        </div>
+        
+        <div class="ablog__text ablog__text4">
+            <div class="ablog__meta ablog__meta4">
+                <ul>
+                    <li>
+                        <a href="<?php the_permalink(); ?>">
+                            <i class="far fa-calendar-check"></i> 
+                            <?php echo esc_html( get_the_date() ); ?>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo get_author_posts_url( get_the_author_meta('ID') ); ?>">
+                            <i class="far fa-user"></i> 
+                            <?php the_author(); ?>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php comments_link(); ?>">
+                            <i class="fal fa-comments"></i> 
+                            <?php comments_number( esc_html__('No Comments', 'airvice'), esc_html__('1 Comment', 'airvice'), esc_html__('% Comments', 'airvice') ); ?>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <h3 class="ablog__text--title4 mb-20">
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </h3>
+
+            <?php the_excerpt(); ?>
+
+            <div class="ablog__btn4">
+                <a href="<?php the_permalink(); ?>" class="theme-btn">
+                    <?php echo esc_html__('Read More', 'airvice'); ?>
+                </a>
+            </div>
+        </div>
+    </div>
+
+<?php endif; ?>
